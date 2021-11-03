86de41fd72fd3b426b447e3410cefb9343c76053
Nicolas Dimitri <nicolasdodimitri@gmail.com>
2021-11-02 23:39:01 -0300

    pokedex

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,36 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            contador: 0,
+        }
+    this.handleChange = this.handleChange.bind(this)
+    }
+handleChange(){
+    const {contador} = this.state
+    if(contador === 8){
+        this.setState({
+            contador: 0,
+        })
+    } else{
+    this.setState(prevState => ({
+        contador: prevState.contador + 1,
+    }))
+    }
+}
     render() {
+        const { 
+            contador,
+        } = this.state
+        const { 
+            pokemons,
+        } = this.props
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon pokemon={pokemons[contador]} />
+            <button onClick={this.handleChange}>--></button>
             </div>
         );
     }
